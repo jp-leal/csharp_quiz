554ecc123b1139370060ea08d24ff4253d71712e
joaobug <joaoenfrance@gmail.com>
2024-05-13 14:57:18 -0300

    quiz game

--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -1,5 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Runtime.CompilerServices;
 
-Console.WriteLine("try to comit in c");
-Console.WriteLine("try to comit in c");
+class QuizProgram
+{
+
+    static void Main() {
+// create questions
+string[] questions = 
+{
+    "For what movie did Steven Spielberg win his first Oscar for Best Director?",
+    "In the movie Babe, what is Babe's role on the farm??",
+    "What is the movie in which Jim Carrey discovers that his everyday life is really a TV show about him??"
+};
+
+string [] answers = 
+{
+    "A. The hunger Games \nB. Schindler's List  \nC. The hunger Games ",
+
+    "A. The hunger Games \nB. Schindler's List  \nC. Sheep herding",
+
+    "A. The hunger Games \nB. Schindler's List  \nC. The Truman Show",
+};
+
+int[] correctAnswers = {1, 2, 2};
+int playerScore = 0;
+
+Console.WriteLine("Welecome to the movies quiz");
+for(int i = 0; i < questions.Length; i++) {
+    Console.WriteLine("Question " + (i + 1));
+    Console.WriteLine(questions[i]);
+    Console.WriteLine(answers[i]);
+    Console.Write("Choose your answer ('A','B','C'): ");
+    String playerAnswer = Console.ReadLine();
+
+    if(string.Equals(playerAnswer, "A", StringComparison.OrdinalIgnoreCase) && correctAnswers[i] == 0) {
+playerScore++;
+    } else if (string.Equals(playerAnswer, "B", StringComparison.OrdinalIgnoreCase) && correctAnswers[i] == 1) {
+playerScore ++;
+    } else if (string.Equals(playerAnswer, "C", StringComparison.OrdinalIgnoreCase) && correctAnswers[i] == 2) {
+playerScore++;
+}}
+Console.WriteLine("YOUR SCORE IS: " + playerScore + "/" + questions.Length);
+    }
+}
--- Quiz/obj/Debug/net8.0/Quiz.AssemblyInfo.cs
+++ Quiz/obj/Debug/net8.0/Quiz.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Quiz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c160dcb0f4008bfc2daf029e634673344402abf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Quiz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Quiz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
